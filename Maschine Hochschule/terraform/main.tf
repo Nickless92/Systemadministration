5bb42a2709f27fa33c451dc67f497d755748c08a
Dominic Meyer <dominic.meyer@gmx.de>
2021-12-22 10:51:01 +0100

    added Terraform

---
+++ Maschine Hochschule/terraform/main.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("ecstatic-pod-334509-7bbcb6d3e043.json")
+
+  project = "ecstatic-pod-334509"
+  region  = "europe-west3"
+  zone    = "europe-west3-b"
+}
+
+
+resource "google_compute_instance" "vm_instance2" {
+  name         = "knoten2"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "terraform-network"
+    access_config {
+    }
+  }
+}
+
+
+resource "google_compute_instance" "vm_instance3" {
+  name         = "knoten3"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "terraform-network"
+    access_config {
+    }
+  }
+}
+
+
+resource "google_compute_firewall" "rules" {
+  project     = "ecstatic-pod-334509"
+  name        = "ssh-firewall"
+  network     = "terraform-network"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["22"]
+  }
+
+  allow {
+    protocol  = "icmp"
+  }
+
+
+#  source_tags = ["foo"]
+#  target_tags = ["web"]
+}
+
+
+
